bb8cf39d46adc180126923455d7b52e51706dd3f
reeselikethepieces <charisse.carlson@gmail.com>
2024-06-28 16:14:54 -0700

    20demo.py

--- 20demo.py
+++ 20demo.py
@@ -57,9 +57,9 @@ print(math.nan)
 
 """
 no_nos
-	print(1/0)				# divide by zero error
-	print(math.log(0))		# math domain error
-	print(math.sqrt(-1))	# math domain error 
+	print(1/0)			# divide by zero error
+	print(math.log(0))			# math domain error
+	print(math.sqrt(-1))		# math domain error 
 	print((-1)**0.5)		# complex number, not a math domain error
 """
 
@@ -162,3 +162,150 @@ def midpt_of_2pts(x1, x2, y1, y2):
 	return (x1+x2)/2, (y2-y1)/2
 
 print(midpt_of_2pts(3, 4, 6, 10))
+
+"""
+strings
+"""
+s = 'hello world' 
+print (s, type(s))
+
+
+"""
+conditionals
+"""
+a = 2
+b = 2
+if a == b:
+	print('a equals b')		#prints bc true
+	print(a, b)				#only reported if both are equal 
+
+'''a = 2 
+b = 3
+if a == b 
+	print('a equals b')
+	print (a, b)			#SyntaxError bc not same/same
+'''	
+a = 5
+b = 5
+if a == b:
+	print('a equals b')		#prints bc true
+print (a, b)				#prints anyways bc outside conditional
+
+a = 3 
+b = 4 
+if a == b:
+	print('a equals b')		#does not print bc the if condition is not met 
+print (a, b)				#prints anyways bc outside conditional 
+
+a = 3 
+b = 4 
+print(a ,b)					#prints anyways bc outside conditional 
+if a == b:
+	print ('a equals b')	#does not print bc the if condition is not met 
+	
+"""
+Boo
+"""
+c = a == b
+print(c)
+print(type(c))
+"""
+change value of a and b
+"""
+
+"""
+if-elif-else
+"""
+if a < b:
+	print('a < b')
+elif a > b:
+	print('a > b')
+else:
+	print('a == b')
+"""
+vs tidy
+"""
+if	 a < b: print ('a < b')
+elif a > b: print ('a > b')
+else:		print ('a ==b')
+
+"""
+chaining
+"""
+if a < b or a > b: print('all things being equal, a and be are not')
+if a < b and a > b: print('you are living in a strange world')
+if not False: print(True)
+
+"""
+Floating Point Warning
+"""
+a = 0.3
+b = 0.1 * 3
+if   a < b: print('a < b')
+elif a > b: print('a > b')
+else:		print('a == b')
+
+print(abs(a - b)) # 5.551115123125783e-17
+if abs(a - b) < 1e-9: print('close enough')
+
+"""
+python math- math fxn 
+	math.isclose() 
+			compares two values and returns
+				if (values are close)
+					print('True')
+				if (^values are close)
+					print('False')
+"""
+if math.isclose(a, b): print('close enough')
+
+"""
+String Comparison
+	somewhat alphabetical - ASCII values 
+		A and a come b4 the letter B 
+			this rings true below
+"""
+s1 = 'A'
+s2 = 'B'
+s3 = 'a'
+if s1 < s2: print('A < B')
+if s2 < s3: print('B < a')
+
+'''
+Mismatched Type Error
+	two variables with different types 
+		make no sense - what do you mean he dont eat no meat? that's okay, I'll make lamb
+			thus, results in "Type Error"
+
+a = 1
+s = 'G'
+if a < s: print('a < s')
+```
+
+
+"""
+more practice problems
+"""
+
+"""
+1. Write a function that describes if a number is an integer. A good name for such a function would 
+be is_integer() or isinteger(). Functions with Boolean return values often have is in their prefix.
+"""
+n = 
+c = math.e, math.pi, math.inf, math.nan 
+def is_integer(n)		# let n == number 
+	if n == c: print('not an integer')
+	
+	return n =
+'''
+
+
+b = 1
+c = 1.0
+d = math.e
+print(type (b))
+
+def is_integer(x):
+	return ()
+		if x == type(b):						print('is an integer')
+		elif x != type(c) or x != type(d):		print('not an integer')
