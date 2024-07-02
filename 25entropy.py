a6d86f768193ab67a1d452343edaa2764f4ec72a
reeselikethepieces <charisse.carlson@gmail.com>
2024-07-01 18:09:57 -0700

    25entropy.py

---
+++ 25entropy.py
@@ -0,0 +1,20 @@
+""" 
+write fxn that returns Shannon entropy (se) for nt counts a,c,t,g 
+	from the internet se = sum probabilities*log2(1/pi) 
+		l
+		E    Pi * log2(1/Pi) 
+	  i = 1
+	  
+where l = total number = 4 
+pi = probability of situation i in the system  = 1/4 = 0.25 
+...thought - bc all four probabilities are the same, cant you calculate se for one nucleotide and then multiply by 4
+to get the total se for nts
+"""
+
+def s_entropy(x):
+	pi == 'A' 
+	nt == 'C'
+	nt == 'G'
+	nt == 'T' 
+	if nt = 4 
+	
