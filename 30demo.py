4b20690bf946d398e310a801397889f1e5ebae38
reeselikethepieces <charisse.carlson@gmail.com>
2024-07-05 14:48:15 -0700

    30demo.py

--- 30demo.py
+++ 30demo.py
@@ -37,13 +37,13 @@ nested loops ~ 31fizzbuzz.py
 		half matrix + major diagonal: inner loop starts at i 
 		half matrix - major diagonal: inner loop starts at i + 1 
 algorithms
-practice problems
-practice solutions
-	triangular()
-	factorial()
-	euler()
-	is_perfect_square()
-	is_prime()
+	most are a mixture of loops and conditionals, and are often contained inside fxns 
+	in ex, create algorithm  with the goal to calculate the GC compo of a nt seq 
+		the fxn needs an input seq, called 'seq' 
+		then, the fxn needs to examine each nt and keep track of how many Gs and Cs it finds
+			keeping track via stored in variable 'gc_count'
+		also need total number of nts, which we'll store in a variable called total 
+	(Ex 6.0 below)
 """
 
 
@@ -179,4 +179,158 @@ B			x	x
 C				x
 D
 symmetrical along the diagonal 
+"""
+
+"""
+Ex 6
+"""
+def gc_comp(seq):
+	gc_count = 0 
+	total = 0 
+	for nt in seq:
+		if nt == 'C' or nt == 'G':
+			gc_count = gc_count + 1
+		total = total + 1
+	return gc_count / total
+	
+print(gc_comp('ACAGCGAAT'))
+
+"""
+most algorithms follow the 3-part structure:
+	1. initialization (lines 187-)
+	2. iteration (lines 197-200)
+	3. Finalization (196) 
+
+	1. gc_comp() has two initializations
+		setting gc_count = 0
+		setting total = 0 
+	2. loop over nt of the seq (nt(seq)) 
+		notice block structure. only when nt's c and g, should gc_count get incremented
+		however, all nts must be counted 
+			tis y gc_count is indented inside the 'if' block but the 'total' is not
+	3. final step is dividing gc_count by total 
+"""
+
+"""
+Practice Problems
+"""
+
+"""
+1. write fxn
+	def fxname(variable):
+2. sum of #'s from 1 to n 
+	i = 1, l = n 
+	for i in range(initial, end-range, increment) 
+				  (0, n, 1) can be simplified to (n)
+3. we know that 21 is the triangular number of 6, so double check the program runs
+"""
+
+def triangular(n):
+	tri = 0 
+	for i in range(n+1):
+		tri = tri + i
+	return tri
+print(triangular(6))
+
+
+"""
+factorial
+"""
+
+
+def factorial(n):
+	if n == 0: return 1
+	fac = 1 
+	for i in range(1, n+1): 
+		fac = fac* i
+	return fac
+	
+print(factorial(4))
+
+"""
+Euler's numbere(2.71828) 
+infinite sum of 1/n! 
+"""
+'''how to print this one to check that it works?
+$import math
+print(euler(math.e))$
+'''
+def euler(limit):
+	e = 0 
+	for n in range(limit):
+		e = e + 1 / factorial(n)
+	return e
+
+"""
+if n == perfect square 
+i.e. 4, 9, 16, 25 are perfect squares bc square roots are integers
+	this means we do not want a remainder or a neg number	
+"""
+import math
+
+def is_perf_sqr(n):
+	root = math.sqrt(n)
+	if root % 1 == 0: return True
+	return False
+	
+print(is_perf_sqr(81))
+print(is_perf_sqr(56))
+print(is_perf_sqr(100))
+'''
+how to make this one work?
+is it even worth it? the idea to call two other existing functions in a new fxn?
+prints None
+
+import math
+
+def is_integer(n):
+	if n % 1 == 0: return True
+	return False
+
+def is_not_complex(n):
+	if n >= 0 and n <= 1: return True
+	return False
+
+def is_perf_sqr(n):
+	if n == is_integer and is_not_complex:
+		return math.sqrt(n) 
+	
+print(is_perf_sqr(81))
+print(is_perf_sqr(56))
+print(is_perf_sqr(100))
+'''
+
+"""
+prime
+google - a whole number greater than 1 that cannot be 
+exactly divided by any whole number other than itself and 1 (e.g. 2, 3, 5, 7, 11).
+"""
+
+'''
+why doesnt this work
+
+def is_prime(n):
+	x != n
+	n >= 1
+	if n // n or n // 1: return True
+	elif n // x: return False
+	return False 
+'''
+
+def is_prime(n):
+	for den in range(2, n+1):
+		if n % den == 0: return False
+	return True 
+
+print(is_prime(5))	#is prime but returns False
+print(is_prime(19)) # ""
+print(is_prime(22))	
+print(is_prime(3))	# ""
+print(is_prime(6))	
+print(is_prime(3))	# ""
+
+
+"""
+This algorithm features a short-circuit (line 3). It returns False as soon as it finds any 
+factor smaller than itself. If it fails to find any factors, it eventually returns True.
 """
